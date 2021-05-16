fbcfe7fa8894d2ac009218a911e2b8a05c3e02e0
Tymoteusz Wrzeszcz <tymoteuszwrzeszcz11@gmail.com>
2021-05-16 09:20:00 +0200

    update

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def index():
 @app.route('/get', methods=['GET', 'POST'])
 def get_bot_response():
     msg = request.args.get('msg')
-    return msg
+    return str(chatbot_response(model, words, msg, intents))
 
 if __name__ == '__main__':
     app.run(debug=True)
